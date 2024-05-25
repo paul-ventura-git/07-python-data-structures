94fbc7225a5564c95e13abbe92a543b8d24f24b6
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-25 14:21:01 -0500

    02collections

--- 01dictionaries.py
+++ 01dictionaries.py
@@ -1,3 +1,6 @@
+# Immutable types like strings and numbers are hashable and work well as dictionary keys.
+# You can also use tuple objects as dictionary keys as long as they contain only hashable types themselves.
+
 phonebook = {
   "bob": 7387,
   "alice": 3719,
---
+++ 02collections.py
@@ -0,0 +1,8 @@
+import collections
+d = collections.OrderedDict(one=1, two=2, three=3)
+
+print(d)
+
+d["four"] = 4
+
+print(d)
