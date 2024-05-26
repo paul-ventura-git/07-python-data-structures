fab63dec08aa8509467f327f67089f816396d502
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-25 19:14:01 -0500

    bytes

---
+++ 07bytes.py
@@ -0,0 +1,8 @@
+# Inmmutable arrays of single bytes
+
+# bytes objects are immutable sequences of single bytes, or integers in the range 0 ≤ x ≤ 255.
+arr = bytes((0, 1, 2, 3))
+
+print(arr[1])
+
+print(arr)
